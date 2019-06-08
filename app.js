3957dc18f776351f080bf18cb31b1adf5fe4a470
Armando Gautreaux <armando.gautreaux@icloud.com>
2019-06-07 23:15:40 -0500

    Added javascript file

---
+++ app.js
@@ -0,0 +1,46 @@
+var myAge = 29; //number
+var myName = 'Armando'; //String
+var isMarried = true; //bolean
+// 0 index  1 index     2
+var mySistersName = ['Alana', 'April'];
+var myFavNumbers = [1, 2, 3, 4, 5];
+
+// console.log(myAge);
+// console.log(myName);
+// console.log(mySistersName[1]);
+// console.log(myFavNumbers[3]);
+// console.log(myFavNumbers.length);
+
+//Object - combination of key-value pairs
+// var favoriteCity = {
+//   name: 'NYC',
+//   population: 50000000,
+//   isLargeCity: true,
+//   favoritePlaces: ['downtown', 'wtc', 'empire State'],
+//   sum: function(c, b) {
+//     console.log(c + b);
+//   }
+// };
+
+// console.log(favoriteCity.population);
+// favoriteCity.sum(3, 4);
+
+//0
+// var cookies = ['cookie1', 'cookie2', 'cookie3', 'cookie4'];
+// for (var i = 0; i < cookies.length; i++) {
+//   console.log('My dog has eaten ' + cookies[i]);
+// }
+
+//functions
+//function expression
+// var sum = function(){
+// console.log(c + b);
+// }
+
+// function sum(c, b) {
+//   console.log(c + b);
+// }
+
+// sum(6, 4);
+
+document.get;
